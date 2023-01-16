3af2cdb22b6b0259b9b7621dec530bf96565c49f
George Santos <george_diu@hotmail.com>
2023-01-15 22:57:04 -0300

    Arquivos modificados para trabalhar com Flex

---
+++ resources/views/painel/painel.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <title>Sofdcaixa</title>
+</head>
+<body>
+    
+  
+<div class="flex-painel">
+  <img src="{{ asset('/assets/imge/SoftDev-Incorporated-logo.png') }}" class="img-fluid img-logo" alt="Sofdcaixa">
+</div>
+
+<div class="flex-painel-abertura-caixa">
+  <div class="flex-caixa-aberto f1">
+
+  </div>
+  <div class="flex-caixa-aberto f2">
+    CAIXA ABERTO 
+    </div>
+</div>
+
+<div class="flex-container">
+<div class="flex-item f-1"> 
+  <img src="{{ asset('/assets/imge/cart-logo-2.jpeg') }}" class="img-thumbnail img-foto-prod">
+</div>
+<div class="flex-item f-2">
+  <div class="flex-elementos e-1">
+    <div class="titulo"> CÓDIGO DE BARRA </div>
+    <div class="infor-dados">XXXXXXXXXXXXXXXXXX</div>
+  </div> 
+
+  <div class="flex-elementos e-1">
+    <div class="titulo">VALOR UNITÁRIO </div>
+    <div class="infor-dados">
+     XXXXXXXXXXXXXXXXXX
+    </div>
+  </div> 
+  
+  <div class="flex-elementos e-1">
+    <div class="titulo"> TOTAL DO ITEM </div>
+    <div class="infor-dados">XXXXXXXXXXXXXXXXXX</div>
+  </div> 
+</div>
+<div class="flex-item f-3">3</div>
+</div>
+
+</body>
+</html>
